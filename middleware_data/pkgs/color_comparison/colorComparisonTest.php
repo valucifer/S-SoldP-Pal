a573adf1fe6339588929c03df345f26cae184369
Nello Saulino <nello.saulino@gmail.com>
2015-11-17 15:31:29 +0100

    Fix ColorComparisonTest

--- middleware_data/pkgs/color_comparison/colorComparisonTest.php
+++ middleware_data/pkgs/color_comparison/colorComparisonTest.php
@@ -9,14 +9,17 @@ $files = scandir("imgs");
 
 $index = 1;
 $start = microtime(true);
-foreach($files as $file){
+
+$color_comparison->getThreeMainColors("VBS0A004,6,005.jpg", true);
+
+/*foreach($files as $file){
     if($file === '.DS_Store' || $file === "." || $file === ".."){
         continue;
     }
     echo "<div>Image ".$index." called ".$file.":</div><br/>";  
     $color_comparison->getThreeMainColors("imgs/".$file, true);
     $index++;
-}
+}*/
 $end = microtime(true)-$start;
 
 echo "<br/>time: ".$end;
